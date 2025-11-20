1dc95f9768fae00f58fb5fb3c92f0d2d3bd65e3c
goekboerue <62758755+goekboerue@users.noreply.github.com>
2025-11-20 17:03:22 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,21 @@
-import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+    // Mevcut çalışma dizinini al (Eski __dirname yerine bunu kullanıyoruz)
+    const root = process.cwd();
+    const env = loadEnv(mode, root, '');
+
     return {
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
       plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
       resolve: {
         alias: {
-          '@': path.resolve(__dirname, '.'),
+          '@': root, // '@' işaretini ana dizine yönlendir
         }
       }
     };
